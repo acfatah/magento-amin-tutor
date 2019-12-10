8d39a279461a0d0858bd39ed50ea6b459669189f
Achmad F. Ibrahim <acfatah@gmail.com>
2019-12-10 00:04:12 +0000

    Extend magento page

--- app/code/Fatah/Hello/Controller/Message/Index.php
+++ app/code/Fatah/Hello/Controller/Message/Index.php
@@ -3,11 +3,21 @@
 namespace Fatah\Hello\Controller\Message;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
 {
+    protected $_pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
     public function execute()
     {
-        echo 'hello world';
+        return $this->_pageFactory->create();
     }
 }
